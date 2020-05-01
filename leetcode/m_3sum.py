1a3771edace664019676ed062df3a608fdaa13ed
Kulraj Suri <kulraj.suri@gmail.com>
2020-05-01 13:16:36 +0100

    three sum complete

--- leetcode/m_3sum.py
+++ leetcode/m_3sum.py
@@ -2,10 +2,22 @@
 # Find all unique triplets in the array which gives the sum of zero
 
 def threeSum(nums):
-    
 
+    Hash = dict()
+    solution = []
 
+    for c1,v1 in enumerate(nums):
+        for c2,v2 in enumerate(nums[c1+1:]):
+            c = -v1-v2
+            Hash[c] = v1,v2
 
+    print(Hash)
+    for i in nums:
+        if i in Hash:
+            solution.append( [ i, Hash[i][0], Hash[i][1] ] )
+
+    #sol2 = set(solution)
+    return solution
 
 
 
