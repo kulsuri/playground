0c9940517036214ece6beae49cdb543eeb1491df
Kulraj Suri <kulraj.suri@gmail.com>
2020-05-07 12:49:12 +0100

    problem 3 complete

--- daily_coding_problem/problem_3.py
+++ daily_coding_problem/problem_3.py
@@ -3,6 +3,7 @@
 # and deserialize(s), which deserializes the string back into the tree
 
 import json
+import pprint
 
 class Node:
     def __init__(self, val, left=None, right=None):
@@ -15,7 +16,32 @@ def serialize(root):
     if not root:
         return None
 
+    serialized_tree_map = dict()
+    serialized_left = serialize(root.left)
+    serialized_right = serialize(root.right)
+
+    serialized_tree_map['data'] = root.val
+
+    if serialized_left:
+        serialized_tree_map['left'] = serialized_left
+    if serialized_right:
+        serialized_tree_map['right'] = serialized_right
     
+    return json.dumps(serialized_tree_map)
+
+def deserialize(serialized_tree_map_json):
+    
+    serialized_tree_map_json = json.loads(serialized_tree_map_json)
+
+    node = Node(serialized_tree_map_json['data'])
+
+    if 'left' in serialized_tree_map_json:
+        node.left = deserialize(serialized_tree_map_json['left'])
+    if 'right' in serialized_tree_map_json:
+        node.right = deserialize(serialized_tree_map_json['right'])
+
+    return node
+
 # initialize nodes
 node_a = Node('a')
 node_b = Node('b')
@@ -33,7 +59,7 @@ node_b.right = node_e
 node_c.left = node_f
 node_c.right = node_g
 
-""" 
+"""
      a
    /  \
   b    c
@@ -41,4 +67,16 @@ node_c.right = node_g
 d  e  f  g
 """ 
 
-print(node_a.val)
+serialized_a = serialize(node_a)
+pprint.pprint(json.loads(serialized_a))
+
+deserialized_a = deserialize(serialized_a)
+
+print(deserialized_a.val) # prints a
+print(deserialized_a.left.val) # prints b
+print(deserialized_a.right.val) # prints c
+print(deserialized_a.left.left.val) # prints d
+print(deserialized_a.left.right.val) # prints e
+
+assert deserialized_a.val == "a"
+assert deserialize( serialize(node_a) ).left.left.val == 'd'
